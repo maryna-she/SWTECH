f07eb663ce29fe35be3809dee74881d3e976c883
Rudii <rudi.sembrum@gmail.com>
2026-06-02 22:14:02 +0200

    kleine Anpassung an den User Klassen

--- backend/src/main/java/com/shopproject/user/UserRepository.java
+++ backend/src/main/java/com/shopproject/user/UserRepository.java
@@ -3,9 +3,11 @@ package com.shopproject.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
-
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
