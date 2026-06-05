aa65b129b3507d87b3226e018b9adbff44fa4779
Finn Kr <fimax24@gmail.com>
2026-06-05 11:40:04 +0200

    Added message for http status not found.

--- backend/src/main/java/com/shopproject/returns/ReturnNotFoundAdvice.java
+++ backend/src/main/java/com/shopproject/returns/ReturnNotFoundAdvice.java
@@ -1,6 +1,19 @@
 package com.shopproject.returns;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
 public class ReturnNotFoundAdvice
 {
+    @ExceptionHandler(ReturnNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String returnNotFoundHandler(ReturnNotFoundException ex)
+    {
+        return ex.getMessage();
+    }
 }
